6dc8c9df3bfda270ca4def3adbda95bb903075d8
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2017-01-22 14:12:34 +0900

    add sign-in function

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-gem 'bootstrap', '~> 4.0.0.alpha3.1'
+gem 'bootstrap', '~> 4.0.0.alpha6'
 gem 'devise'
 gem 'devise-i18n'
 gem 'pg'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    registrations: 'users/registrations',
+    passwords:     'users/passwords'
+  }
+  devise_scope :user do
+    authenticated :user do
+      root to: 'tips#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root to: 'users/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
